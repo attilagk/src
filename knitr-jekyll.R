c934abb1e566a116c22d9d68527c1a9aee0e63bc
Attila <attila.gulyas.kovacs@gmail.com>
2017-05-27 18:57:30 -0400

    Evaluation during knitting in globalenv

--- knitr-jekyll.R
+++ knitr-jekyll.R
@@ -1,6 +1,6 @@
 # knit rmd to HTML and build jekyll site in site.dir
 knit.build <- function(rmd, site.dir='~/lab-notebook') {
-    knit2html(rmd, fragment.only=TRUE)
+    knit2html(rmd, fragment.only=TRUE, envir=globalenv())
     save.dir <- getwd()
     setwd(site.dir)
     system2('bundle', args= c('exec', 'jekyll', 'build'))
