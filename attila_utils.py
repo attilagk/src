dc1f99205d0efab78569891411a3bc1e9e296255
Attila Jones <attila.g.jones@gmail.com>
2020-10-09 11:22:16 -0400

    update on savefig

--- attila_utils.py
+++ attila_utils.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+import subprocess
 
 def savefig(fig, basename, dirpath='named-figure/', formats=['png', 'pdf']):
     '''
@@ -18,7 +19,23 @@ def savefig(fig, basename, dirpath='named-figure/', formats=['png', 'pdf']):
     bpath = dirpath + basename
     def save_one(fm):
         filepath = bpath + '.' + fm
-        fig.savefig(fname=filepath, format=fm)
+        fig.savefig(fname=filepath, format=fm, bbox_inches="tight")
         return(filepath)
     res = [save_one(fm) for fm in formats]
     return(res)
+
+
+def sentieon_licsrvr():
+    args0 = ['pgrep', '-f', 'licsrvr']
+    proc0 = subprocess.run(args0)
+    if proc0.returncode == 0:
+        # license server is already running
+        return(proc0)
+    sentieon_lic = os.environ['SENTIEON_LICENSE']
+    sentieon_dir = os.environ['SENTIEON_INSTALL_DIR']
+    sentieon_exec = sentieon_dir + os.path.sep + 'bin/sentieon'
+    sentieon_log = '/home/attila/tools/sentieon.log'
+    args1 = [sentieon_exec, 'licsrvr', '--start', '--log', sentieon_log, sentieon_lic]
+    proc1 = subprocess.run(args1)
+    return(proc1)
+
